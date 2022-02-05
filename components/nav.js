d3da7f6a6c5032ec4422af83f1d39cbcc97f9442
mayur <mayur072000@gmail.com>
2022-02-05 17:47:07 +0530

    add: theme toggling

--- components/nav.js
+++ components/nav.js
@@ -15,6 +15,7 @@ import {
   useColorModeValue
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
+import ThemeButton from './theme-button'
 
 const LinkItem = ({ href, path, children }) => {
   const active = path == href
@@ -87,6 +88,7 @@ const Navbar = props => {
         </Stack>
 
         <Box flex={1} align="right">
+          <ThemeButton/>
           <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
             <Menu isLazy id="navbar-menu">
               <MenuButton
