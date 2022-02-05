28d0298e31936b2da58b9ab4341ac24ce28841f3
mayur <mayur072000@gmail.com>
2022-02-05 23:31:38 +0530

    add: profile picture

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import { Container, Heading, Box } from "@chakra-ui/react"
-import theme from "../libs/theme"
+import { Container, Heading, Box, Image, useColorModeValue} from '@chakra-ui/react'
+import theme from '../libs/theme'
 
 const Page = () => {
   //TODO: Remove placeholder text and add correct details and description
@@ -14,6 +14,23 @@ const Page = () => {
             Mayur Bhoi
           </Heading>
           <p>Software Developer</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="lg"
+            src="/images/me.png"
+            alt="Profile Picture"
+          />
         </Box>
       </Box>
     </Container>
