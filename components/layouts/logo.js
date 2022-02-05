877bfa6b457f8fc4d04ba37f6e0b7d1cd12f0c78
mayur <mayur072000@gmail.com>
2022-02-05 14:01:55 +0530

    add: navbar and images

---
+++ components/layouts/logo.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Text, useColorModeValue } from '@chakra-ui/react'
+import styled from "@emotion/styled";
+
+const LogoBox = styled.span`
+font-weight: bold;
+font-size: 18px;
+display: inline-flex;
+align-items: center;
+height: 30px;
+line-height: 20px;
+padding: 10px;
+opacity: 0.6
+
+&:hover img {
+    transform: rotate(15deg);
+    opacity: 0.9
+}
+`
+
+const Logo = () => {
+    const logoImg = `/images/logo${useColorModeValue('', '-dark')}.png)`
+
+      return (
+        <Link href="/">
+          <a>
+            <LogoBox>
+              <Image src={logoImg} width={20} height={20} alt="logo" />
+              <Text
+                color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+                fontFamily='M PLUS Rounded 1c", sans-serif'
+                fontWeight="bold"
+                ml={3}
+              >
+                Mayur Bhoi
+              </Text>
+            </LogoBox>
+          </a>
+        </Link>
+      )
+}
+
+export default Logo
+
---
+++ components/layouts/nav.js
@@ -0,0 +1,61 @@
+import Logo from './logo'
+import NextLink from 'next/link'
+import {
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+
+const LinkItem = ({ href, path, children }) => {
+  const active = path == href
+  const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+  return (
+    <NextLink href={href}>
+      <Link
+        p={2}
+        bg={active ? 'glassTeal' : undefined}
+        color={active ? '#202023' : inactiveColor}
+      >
+        {children}
+      </Link>
+    </NextLink>
+  )
+}
+
+const Navbar = props => {
+  const { path } = props
+  return (
+    <Box
+      position="fixed"
+      as="nav"
+      w="100%"
+      bg={useColorModeValue('#ffffff40', '#20202380')}
+      style={{ backdropFilter: 'blur(10px)' }}
+      zIndex={1}
+      {...props}
+    >
+      <Container
+        display="flex"
+        p={2}
+        maxW="container.md"
+              wrap="wrap"
+              align="center"
+              justify="space-between"
+          >
+              //TODO: add components
+      </Container>
+    </Box>
+  )
+}
+
+export default Navbar
