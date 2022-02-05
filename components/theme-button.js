bdb93ee73a80d954ef59d556e49cca703eb637a0
mayur <mayur072000@gmail.com>
2022-02-05 17:45:25 +0530

    setup theme toggling

--- components/theme-button.js
+++ components/theme-button.js
@@ -1,1 +1,17 @@
-import { IconButton } from "@chakra-ui/react";
+import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { SunIcon, MoonIcon } from '@chakra-ui/icons'
+
+const ThemeButton = () => {
+  const { toggleColorMode } = useColorMode()
+
+  return (
+    <IconButton
+      aria-label="Theme toggle"
+      colorScheme={useColorModeValue('purple', 'orange')}
+      icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+      onClick={toggleColorMode}
+    ></IconButton>
+  )
+}
+
+export default ThemeButton
