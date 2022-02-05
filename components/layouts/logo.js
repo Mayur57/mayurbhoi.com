842a297899c6c58926b368a9a315ba8aa6a3f1e4
mayur <mayur072000@gmail.com>
2022-02-05 14:23:28 +0530

    navbar: logo shenanigans

--- components/layouts/logo.js
+++ components/layouts/logo.js
@@ -4,19 +4,24 @@ import { Text, useColorModeValue } from '@chakra-ui/react'
 import styled from "@emotion/styled";
 
 const LogoBox = styled.span`
-font-weight: bold;
-font-size: 18px;
-display: inline-flex;
-align-items: center;
-height: 30px;
-line-height: 20px;
-padding: 10px;
-opacity: 0.6
+  font-weight: bold;
+  font-size: 18px;
+  display: inline-flex;
+  align-items: center;
+  height: 30px;
+  line-height: 20px;
+  padding: 10px;
+  opacity: 1;
 
-&:hover img {
-    transform: rotate(15deg);
-    opacity: 0.9
-}
+  img {
+    transition: 300ms ease all;
+  }
+
+  &:hover img {
+    transform: rotate(90deg);
+    opacity: 0.5;
+    transition: 300ms ease all;
+  }
 `
 
 const Logo = () => {
@@ -31,7 +36,7 @@ const Logo = () => {
                 color={useColorModeValue('gray.800', 'whiteAlpha.900')}
                 fontFamily='"Inter", sans-serif'
                 fontWeight="bold"
-                ml={3}
+                ml={2}
               >
                 Mayur Bhoi
               </Text>
