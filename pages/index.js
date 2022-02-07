d65f3b6f24d9305d44168f16e4e38963d2d8bddc
mayur <mayur072000@gmail.com>
2022-02-07 12:01:17 +0530

    fix: desc

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,7 @@ const Page = () => {
         </Heading>
         <Paragraph>
           I am a self-taught full-stack developer with a rich background in
-          developing mobile apps. I have a knack for writing intricatly
+          developing mobile apps. I have a knack for writing intricately
           designed, responsive software to solve problems. I like writing
           scripts to automate my tasks and routine.
         </Paragraph>
