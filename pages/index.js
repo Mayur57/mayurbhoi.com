54c51020300b90f6db79e1f8d8020003076f8793
mayur <mayur072000@gmail.com>
2022-04-19 17:12:10 +0530

    add: new pre-release warning

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Container, useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  VStack,
+  Text,
+  useToast,
+  HStack,
+  IconButton,
+  CloseButton,
+} from "@chakra-ui/react";
 import { useEffect } from "react";
+import { InfoIcon } from "@chakra-ui/icons";
 import { GridItemStyle } from "../components/grid-item";
 import HeaderSection from "../components/index/HeaderSection";
 import AboutParaSection from "../components/index/AboutParaSection";
@@ -13,13 +23,48 @@ const Page = () => {
   useEffect(() => {
     if (process.env.NEXT_PUBLIC_PHASE === "pre-release") {
       toast({
-        title: "Pre-release Website",
-        description:
-          "The website is still in pre-release phase. Functionality maybe unstable and content may not be final yet.",
-        status: "error",
-        duration: 9000,
-        variant: "left-accent",
-        isClosable: true,
+        render: () => (
+          <Box
+            color="white"
+            p={4}
+            maxWidth="300px"
+            borderRadius={8}
+            bg="red.500"
+            filter="auto"
+            borderWidth={1}
+            borderColor="red.400"
+            dropShadow="7px 7px 10px #00000050"
+          >
+            <IconButton
+              icon={<CloseButton />}
+              pos="absolute"
+              right={0}
+              top={1.5}
+              backgroundColor="transparent"
+              p={0}
+              maxWidth={2}
+              height={6}
+              onClick={() => {
+                toast.closeAll();
+              }}
+              _hover={{ backgroundColor: "transparent" }}
+            />
+            <HStack align="start" pr={4}>
+              <InfoIcon mt={1} />
+              <VStack spacing={-1} alignItems="left" pl={2}>
+                <Text fontSize={13} fontWeight="bold">
+                  Pre-release Version
+                </Text>
+                <Text fontSize={12} pt={2}>
+                  The website is still in pre-release phase. Functionality maybe
+                  unstable and content may not be final yet.
+                </Text>
+              </VStack>
+            </HStack>
+          </Box>
+        ),
+        duration: 15000,
+        position: "top-right",
       });
     }
   }, []);
