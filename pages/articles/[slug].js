1bb5f8826acc97816b6c42939167b82342caa2aa
mayur <mayur072000@gmail.com>
2022-05-22 05:46:09 +0530

    fix: pre-rendering issues with dynamic routes

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -9,27 +9,34 @@ import {
   Stack,
   HStack,
 } from "@chakra-ui/react";
-import { useRouter } from "next/router";
 import Image from "next/image";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { Global } from "@emotion/react";
 import { RiErrorWarningLine } from "react-icons/ri";
-import ErrorPage from "next/error";
 import { Meta } from "../../components/work";
 import P from "../../components/paragraph";
 import Layout from "../../components/layouts/article";
 import Images from "../../data/images";
+import articles from "../../data/blogs";
 
-const articles = [
-  {
-    title: "Twitter Nuke",
-    description:
-      "A Twitter bot that connects people willing to help with those in need. Written in Python 3.",
-    slug: "twitter-nuke",
-    image: Images.covid,
-    published: "02 feb 2020",
-  },
-];
+export const getStaticProps = async ({ params }) => {
+  const articlesList = articles.filter(
+    (article) => article.slug.toString() === params.slug
+  );
+  return {
+    props: {
+      article: articlesList[0],
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const paths = articles.map((article) => ({
+    params: { slug: article.slug.toString() },
+  }));
+
+  return { paths, fallback: false };
+};
 
 const SectionTitle = ({ children }) => (
   <Heading variant="pronouns" fontWeight={500} fontSize={13} mt={4} mb={2}>
@@ -55,77 +62,68 @@ const DeveloperWarning = () => (
   </Box>
 );
 
-const Work = () => {
-  const router = useRouter();
-  if (!router.isFallback) {
-    return <ErrorPage statusCode={404} />;
-  }
-  const { slug } = router.query;
-  const filterResult = articles.filter((project) => project.slug === slug);
-  const project = filterResult[0];
-  return (
-    <Layout title={project.title}>
-      <Container maxW="container.lg" mt={4}>
-        <DeveloperWarning />
-        <Stack display="flex" direction={{ md: "row", base: "column" }}>
-          <Box flex={2} mr={{ base: 40, sm: 0, md: 40, xl: 60 }}>
-            <Heading variant="pronouns" fontWeight={500} fontSize={13} mt={4}>
-              Project
-            </Heading>
-            <Text fontWeight={800} letterSpacing={-2} fontSize={48}>
-              {project.title}
-            </Text>
-            <Heading variant="pronouns" fontWeight={500} fontSize={13} mt={4}>
-              Description
-            </Heading>
-            <P>{project.description}</P>
-          </Box>
-          <Box flex={2}>
-            {/** //! TODO: Do not use this in a real project doc */}
-            <SectionTitle>About</SectionTitle>
-            <P>
-              In publishing and graphic design, Lorem ipsum is a placeholder
-              text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. Lorem ipsum may
-              be used as a placeholder before the final copy is available.
-            </P>
-            <SectionTitle>Stack</SectionTitle>
-            <P>
-              In publishing and graphic design, Lorem ipsum is a placeholder
-              text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. Lorem ipsum may
-              be used as a placeholder before the final copy is available.
-            </P>
-            <SectionTitle>Learnings</SectionTitle>
-            <P>
-              In publishing and graphic design, Lorem ipsum is a placeholder
-              text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. Lorem ipsum may
-              be used as a placeholder before the final copy is available.
-            </P>
-            <SectionTitle>Links</SectionTitle>
-            <List ml={4} my={4}>
-              <ListItem>
-                <Meta>Website</Meta>
-                <Link href="/">
-                  https://dummy.website.com
-                  <ExternalLinkIcon ml="8px" mb="2px" fontSize={12} />
-                </Link>
-              </ListItem>
-            </List>
-          </Box>
-        </Stack>
-        <SectionTitle>Product Images</SectionTitle>
-        <Image
-          src={Images.covid}
-          alt="Product image"
-          className="pimage"
-          placeholder="blur"
-        />
-      </Container>
-    </Layout>
-  );
-};
+const Work = ({ article }) => (
+  <Layout title={article.title}>
+    <Container maxW="container.lg" mt={4}>
+      <DeveloperWarning />
+      <Stack display="flex" direction={{ md: "row", base: "column" }}>
+        <Box flex={2} mr={{ base: 40, sm: 0, md: 40, xl: 60 }}>
+          <Heading variant="pronouns" fontWeight={500} fontSize={13} mt={4}>
+            Project
+          </Heading>
+          <Text fontWeight={800} letterSpacing={-2} fontSize={48}>
+            {article.title}
+          </Text>
+          <Heading variant="pronouns" fontWeight={500} fontSize={13} mt={4}>
+            Description
+          </Heading>
+          <P>{article.description}</P>
+        </Box>
+        <Box flex={2}>
+          {/** //! TODO: Do not use this in a real project doc */}
+          <SectionTitle>About</SectionTitle>
+          <P>
+            In publishing and graphic design, Lorem ipsum is a placeholder text
+            commonly used to demonstrate the visual form of a document or a
+            typeface without relying on meaningful content. Lorem ipsum may be
+            used as a placeholder before the final copy is available.
+          </P>
+          <SectionTitle>Stack</SectionTitle>
+          <P>
+            In publishing and graphic design, Lorem ipsum is a placeholder text
+            commonly used to demonstrate the visual form of a document or a
+            typeface without relying on meaningful content. Lorem ipsum may be
+            used as a placeholder before the final copy is available.
+          </P>
+          <SectionTitle>Learnings</SectionTitle>
+          <P>
+            In publishing and graphic design, Lorem ipsum is a placeholder text
+            commonly used to demonstrate the visual form of a document or a
+            typeface without relying on meaningful content. Lorem ipsum may be
+            used as a placeholder before the final copy is available.
+          </P>
+          <SectionTitle>Links</SectionTitle>
+          <List ml={4} my={4}>
+            <ListItem>
+              <Meta>Website</Meta>
+              <Link href="/">
+                https://dummy.website.com
+                <ExternalLinkIcon ml="8px" mb="2px" fontSize={12} />
+              </Link>
+            </ListItem>
+          </List>
+        </Box>
+      </Stack>
+      <SectionTitle>Product Images</SectionTitle>
+      <Image
+        src={Images.covid}
+        alt="Product image"
+        className="pimage"
+        placeholder="blur"
+      />
+    </Container>
+  </Layout>
+);
 export default Work;
 
 export const ProductImageStyle = () => (
--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -9,68 +9,34 @@ import {
   Stack,
   HStack,
 } from "@chakra-ui/react";
-import { useRouter } from "next/router";
 import Image from "next/image";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { Global } from "@emotion/react";
 import { RiErrorWarningLine } from "react-icons/ri";
-import ErrorPage from "next/error";
 import { Meta } from "../../components/work";
 import P from "../../components/paragraph";
 import Layout from "../../components/layouts/article";
 import Images from "../../data/images";
+import projects from "../../data/projects";
 
-const projects = [
-  {
-    title: "Project Bittchje",
-    description:
-      "A Twitter bot that connects people willing to help with those in need. Written in Python 3.",
-    slug: "twitter-nuke1",
-    image: Images.covid,
-  },
-  {
-    title: "Project 2",
-    description:
-      "A Twitter bot that connects people willing to help with those in need. Written in Python 3.",
-    slug: "twitter-nuke2",
-    image: Images.covid,
-  },
-  {
-    title: "Project 3",
-    description:
-      "A Twitter bot that connects people willing to help with those in need. Written in Python 3.",
-    slug: "twitter-nuke3",
-    image: Images.covid,
-  },
-  {
-    title: "Project 4",
-    description:
-      "A Twitter bot that connects people willing to help with those in need. Written in Python 3.",
-    slug: "twitter-nuke4",
-    image: Images.covid,
-  },
-  {
-    title: "Project 5",
-    description:
-      "A Twitter bot that connects people willing to help with those in need. Written in Python 3.",
-    slug: "twitter-nuke5",
-    image: Images.covid,
-  },
-  {
-    title: "Project 6",
-    description:
-      "A Twitter bot that connects people willing to help with those in need. Written in Python 3.",
-    slug: "twitter-nuke6",
-    image: Images.covid,
-  },
-  {
-    title: "Project 7",
-    description:
-      "A Twitter bot that connects people willing to help with those in need. Written in Python 3.",
-    slug: "twitter-nuke7",
-    image: Images.covid,
-  },
-];
+export const getStaticProps = async ({ params }) => {
+  const projectsList = projects.filter(
+    (project) => project.slug.toString() === params.slug
+  );
+  return {
+    props: {
+      project: projectsList[0],
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const paths = projects.map((project) => ({
+    params: { slug: project.slug.toString() },
+  }));
+
+  return { paths, fallback: false };
+};
 
 const SectionTitle = ({ children }) => (
   <Heading variant="pronouns" fontWeight={500} fontSize={13} mt={4} mb={2}>
@@ -96,86 +62,77 @@ const DeveloperWarning = () => (
   </Box>
 );
 
-const Work = () => {
-  const router = useRouter();
-  if (!router.isFallback) {
-    return <ErrorPage statusCode={404} />;
-  }
-  const { slug } = router.query;
-  const filterResult = projects.filter((project) => project.slug === slug);
-  const project = filterResult[0];
-  return (
-    <Layout title={project.title}>
-      <Container maxW="container.lg" mt={4}>
-        <DeveloperWarning />
-        <Stack display="flex" direction={{ md: "row", base: "column" }}>
-          <Box flex={2} mr={{ base: 40, sm: 0, md: 30, xl: 36 }}>
-            <Heading
-              variant="pronouns"
-              fontWeight={500}
-              fontSize={13}
-              mt={4}
-              opacity={0.6}
-            >
-              Project
-            </Heading>
-            <Text fontWeight={800} letterSpacing={-2} fontSize={48}>
-              {project.title}
-            </Text>
-            <Heading variant="pronouns" fontWeight={500} fontSize={13} mt={12}>
-              Description
-            </Heading>
-            <P>{project.description}</P>
-            <Heading variant="pronouns" fontWeight={500} fontSize={13} mt={12}>
-              Description
-            </Heading>
-            <P>{project.description}</P>
-          </Box>
-          <Box flex={3}>
-            <SectionTitle>About</SectionTitle>
-            <P>
-              In publishing and graphic design, Lorem ipsum is a placeholder
-              text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. Lorem ipsum may
-              be used as a placeholder before the final copy is available.
-            </P>
-            <SectionTitle>Stack</SectionTitle>
-            <P>
-              In publishing and graphic design, Lorem ipsum is a placeholder
-              text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. Lorem ipsum may
-              be used as a placeholder before the final copy is available.
-            </P>
-            <SectionTitle>Learnings</SectionTitle>
-            <P>
-              In publishing and graphic design, Lorem ipsum is a placeholder
-              text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. Lorem ipsum may
-              be used as a placeholder before the final copy is available.
-            </P>
-            <SectionTitle>Links</SectionTitle>
-            <List ml={4} my={4}>
-              <ListItem>
-                <Meta>Website</Meta>
-                <Link href="/">
-                  https://dummy.website.com
-                  <ExternalLinkIcon ml="8px" mb="2px" fontSize={12} />
-                </Link>
-              </ListItem>
-            </List>
-          </Box>
-        </Stack>
-        <SectionTitle>Product Images</SectionTitle>
-        <Image
-          src={Images.covid}
-          alt="Product image"
-          className="pimage"
-          placeholder="blur"
-        />
-      </Container>
-    </Layout>
-  );
-};
+const Work = ({ project }) => (
+  <Layout title={project.title}>
+    <Container maxW="container.lg" mt={4}>
+      <DeveloperWarning />
+      <Stack display="flex" direction={{ md: "row", base: "column" }}>
+        <Box flex={2} mr={{ base: 40, sm: 0, md: 30, xl: 36 }}>
+          <Heading
+            variant="pronouns"
+            fontWeight={500}
+            fontSize={13}
+            mt={4}
+            opacity={0.6}
+          >
+            Project
+          </Heading>
+          <Text fontWeight={800} letterSpacing={-2} fontSize={48}>
+            {project.title}
+          </Text>
+          <Heading variant="pronouns" fontWeight={500} fontSize={13} mt={12}>
+            Description
+          </Heading>
+          <P>{project.description}</P>
+          <Heading variant="pronouns" fontWeight={500} fontSize={13} mt={12}>
+            Description
+          </Heading>
+          <P>{project.description}</P>
+        </Box>
+        <Box flex={3}>
+          <SectionTitle>About</SectionTitle>
+          <P>
+            In publishing and graphic design, Lorem ipsum is a placeholder text
+            commonly used to demonstrate the visual form of a document or a
+            typeface without relying on meaningful content. Lorem ipsum may be
+            used as a placeholder before the final copy is available.
+          </P>
+          <SectionTitle>Stack</SectionTitle>
+          <P>
+            In publishing and graphic design, Lorem ipsum is a placeholder text
+            commonly used to demonstrate the visual form of a document or a
+            typeface without relying on meaningful content. Lorem ipsum may be
+            used as a placeholder before the final copy is available.
+          </P>
+          <SectionTitle>Learnings</SectionTitle>
+          <P>
+            In publishing and graphic design, Lorem ipsum is a placeholder text
+            commonly used to demonstrate the visual form of a document or a
+            typeface without relying on meaningful content. Lorem ipsum may be
+            used as a placeholder before the final copy is available.
+          </P>
+          <SectionTitle>Links</SectionTitle>
+          <List ml={4} my={4}>
+            <ListItem>
+              <Meta>Website</Meta>
+              <Link href="/">
+                https://dummy.website.com
+                <ExternalLinkIcon ml="8px" mb="2px" fontSize={12} />
+              </Link>
+            </ListItem>
+          </List>
+        </Box>
+      </Stack>
+      <SectionTitle>Product Images</SectionTitle>
+      <Image
+        src={Images.covid}
+        alt="Product image"
+        className="pimage"
+        placeholder="blur"
+      />
+    </Container>
+  </Layout>
+);
 export default Work;
 
 export const ProductImageStyle = () => (
