3a66dfcae50190c3ca4baaecdb6467fbca023070
mayur <mayur072000@gmail.com>
2022-08-29 01:05:46 +0530

    add: remote file caching

--- next.config.js
+++ next.config.js
@@ -7,4 +7,18 @@ module.exports = {
   images: {
     domains: ["i.ibb.co", "via.placeholder.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png|webp|css|woff2|woff)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
