736ea80579f3e2e9523114c96fb16dc037eee8fc
Mayur Bhoi <mayur072000@gmail.com>
2023-06-26 21:52:10 +0530

    fix: fonts fucked

--- components/Title.js
+++ components/Title.js
@@ -1,10 +1,18 @@
+/* eslint-disable camelcase */
 /* eslint-disable react/jsx-props-no-spreading */
 import { Heading, useColorModeValue } from "@chakra-ui/react";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
+import { Kaisei_Tokumin } from "next/font/google"; 
 
-const Kaisei = localFont({
-  fontFamily: 'Kaisei Tokumin',
-  src: '../public/fonts/KaiseiTokumin-Bold.ttf',
+// const Kaisei = localFont({
+//   fontFamily: 'Kaisei Tokumin',
+//   src: '../public/fonts/KaiseiTokumin-Bold.ttf',
+// });
+
+const bro = Kaisei_Tokumin({
+  variable: '--font-local',
+  subsets: ['latin'],
+  weight: '700',
 });
 
 const Title = (props) => (
@@ -15,7 +23,7 @@ const Title = (props) => (
   color={useColorModeValue("#281D1A", "#EDEDED")}
   mt={{ base: 0, sm: 12 }}
   mb={8}
-  {...Kaisei.style}
+  {...bro.style}
   {...props}
 />
 );
