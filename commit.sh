98a43a36679bf29ee6368266cacb51a7c2a8f404
Mayur Bhoi <mayur072000@gmail.com>
2023-07-19 12:08:51 +0530

    new

--- commit.sh
+++ commit.sh
@@ -20,3 +20,20 @@ git commit -m "${COM_MSG}"
 git push origin "${BRANCH}"
 
 echo -e "\n👍 Commit made to ${CBRANCH} successfully: ${GREEN}${COM_MSG}${NC}"
+
+
+gcb() {
+    echo "✅ Intialising feature commit script..."
+    GREEN='\033[0;32m'
+    NC='\033[0m'
+    echo -n "Enter the branch: "
+    # read BRANCH
+    CBRANCH="${GREEN}$1${NC}"
+    echo -e "\n> The selected branch is: ${CBRANCH}"
+    echo -e "\n> Enter the commit message: "
+    read $2
+    git add .
+    git commit -m "$2"
+    git push origin "$1"
+    echo -e "\n👍 Commit made to ${CBRANCH} successfully: ${GREEN}$2${NC}"
+}
