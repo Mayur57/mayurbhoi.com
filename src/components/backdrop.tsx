6f63b9a3c773afdb54592c496a4e70c470c4cba9
Mayur Bhoi <mayur072000@gmail.com>
2023-12-28 19:40:16 +0530

    refactor: simplify images and backdrop

--- src/components/backdrop.tsx
+++ src/components/backdrop.tsx
@@ -1,18 +1,14 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
-import { motion } from 'framer-motion'
 
 const GridBackdrop = () => {
   const path = usePathname()
   return (
     path !== '/' && (
-      <motion.div
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ ease: [0.25, 0.1, 0.25, 1.0], duration: 1 }}>
+      <div>
         <div className='absolute inset-0 h-60 w-full bg-white dark:bg-[#121212] bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,#80808025_1px,transparent_1px),linear-gradient(to_bottom,#80808025_1px,transparent_1px)] bg-[size:36px_36px] [mask-image:linear-gradient(to_bottom,#FFF,transparent)]' />
-      </motion.div>
+      </div>
     )
   )
 }
