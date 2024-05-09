2703e36fbc35e3926e3934e333a3d50cc279007a
Mayur Bhoi <mayur072000@gmail.com>
2024-05-09 20:32:33 +0530

    post: about locks and encryption

--- src/app/posts/post-tile.tsx
+++ src/app/posts/post-tile.tsx
@@ -5,7 +5,7 @@ import { stagger } from 'src/utils/functions'
 const PostTile = ({ post, index }: { post: any; index: number }) => (
   <Link href={`/posts/${post.slug}`}>
     <div
-      className={`appear ${stagger(index > 7 ? 7 : index)} flex items-center justify-start px-2 py-3 gap-6 dark:hover:bg-[#232323] hover:bg-[#EAEAEA] cursor-ne-resize`}>
+      className={`appear ${stagger(index > 3 ? 5 : index)} flex items-center justify-start px-2 py-3 gap-6 dark:hover:bg-[#232323] hover:bg-[#EAEAEA] cursor-ne-resize`}>
       <div>
         <p className='font-mono font-medium text-xs opacity-50'>{post.tag}</p>
         <p className='font-mono font-medium text-xs opacity-50'>
