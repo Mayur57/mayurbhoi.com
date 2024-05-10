fc669bb7123b0256d8207aaa6def7d0d0e3c84bc
Mayur Bhoi <mayur072000@gmail.com>
2024-05-10 18:56:11 +0530

    add: new og images

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -7,7 +7,9 @@ export async function GET(req: NextRequest): Promise<any> {
   const { searchParams } = req.nextUrl
   const title = searchParams.get('title')
   const twitter = searchParams.get('twitter')
-  const imageUrl = twitter !== ""
+  const isForTwitter = twitter !== null
+  console.log({ isForTwitter }, { twitter })
+  const imageUrl = isForTwitter
     ? 'https://www.mayurbhoi.com/images/og-t.png'
     : 'https://www.mayurbhoi.com/images/og.png'
   const font = fetch(
@@ -25,8 +27,8 @@ export async function GET(req: NextRequest): Promise<any> {
           height: '100%',
           width: '100%',
           display: 'flex',
-          paddingLeft: '4.5rem',
-          paddingTop: '4.5rem',
+          paddingLeft: isForTwitter ? '3rem' : '4.5rem',
+          paddingTop: isForTwitter ? '3rem' : '4.5rem',
           color: '#423939',
           flexDirection: 'column',
           alignItems: 'flex-start',
@@ -37,14 +39,14 @@ export async function GET(req: NextRequest): Promise<any> {
         <div
           style={{
             display: 'flex',
-            fontSize: 88,
+            fontSize: isForTwitter ? 70 : 88,
             letterSpacing: '-0.07em',
             paddingBottom: '1rem',
             fontStyle: 'normal',
-            lineHeight: '100px',
+            lineHeight: isForTwitter ? '85px' : '100px',
             whiteSpace: 'pre-wrap',
-            maxWidth: '789px',
-            maxHeight: '420px',
+            maxWidth: isForTwitter ? '701px' : '789px',
+            maxHeight: isForTwitter ? '289px' : '420px',
             fontWeight: '500',
             overflow: 'hidden',
           }}>
@@ -53,8 +55,8 @@ export async function GET(req: NextRequest): Promise<any> {
       </div>
     ),
     {
-      width: 1200,
-      height: 630,
+      width: isForTwitter ? 1024 : 1200,
+      height: isForTwitter ? 512 : 630,
       fonts: [
         {
           name: 'Inter',
