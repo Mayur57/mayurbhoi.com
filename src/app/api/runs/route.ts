c2df608d699ac6167a117644010d545bde823a35
Mayur Bhoi <mayur072000@gmail.com>
2024-06-25 23:38:04 +0530

    fix: dynamic cron job error

--- src/app/api/runs/route.ts
+++ src/app/api/runs/route.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from 'next/server'
 import { getActivities } from 'src/processor/strava'
+import { headers } from 'next/headers'
 
 export async function GET(req: Request) {
   try {
-    if (req.headers.get('Authorization') !== `Bearer ${process.env['CRON']}`)
+    const headerList = headers()
+    if (headerList.get('Authorization') !== `Bearer ${process.env['CRON']}`)
       return NextResponse.json({ error: 'Unauthorised mate' })
     const activities = await getActivities()
     return NextResponse.json({ data: activities })
