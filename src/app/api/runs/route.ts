b34e3a11ebe3e4d0a90456cc108e47acd717c605
Mayur Bhoi <mayur072000@gmail.com>
2024-06-25 23:35:07 +0530

    format: api

--- src/app/api/runs/route.ts
+++ src/app/api/runs/route.ts
@@ -3,12 +3,10 @@ import { getActivities } from 'src/processor/strava'
 
 export async function GET(req: Request) {
   try {
-    if (req.headers.get('Authorization') === `Bearer ${process.env['CRON']}`) {
-      const activities = await getActivities()
-      return NextResponse.json({ data: activities })
-    } else {
+    if (req.headers.get('Authorization') !== `Bearer ${process.env['CRON']}`)
       return NextResponse.json({ error: 'Unauthorised mate' })
-    }
+    const activities = await getActivities()
+    return NextResponse.json({ data: activities })
   } catch (error: any) {
     return NextResponse.json({ error: error.message })
   }
