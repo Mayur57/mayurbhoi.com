6adca3bafed1f83c3bed38237cdb4147e473a6bd
Mayur Bhoi <mayur072000@gmail.com>
2024-08-24 02:06:25 +0530

    add: visitor location

--- src/components/visitor.tsx
+++ src/components/visitor.tsx
@@ -23,19 +23,16 @@ export const LastVisitor = () => {
         setLocation(location)
 
         // Fetch current location and store in KV
-        const ipRes = await fetch('http://ip-api.com/json')
+        const ipRes = await fetch('https://ipapi.co/json')
         const ipData = await ipRes.json()
-        if (ipData.status === 'success') {
-          const { city, country } = ipData
-          await fetch('/api/location', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': '*',
-            },
-            body: JSON.stringify({ city, country }),
-          })
-        }
+        const { city, country_name } = ipData
+        await fetch('/api/location', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ city, country: country_name }),
+        })
       } catch (error) {
         console.error('Error fetching or updating location:', error)
       }
