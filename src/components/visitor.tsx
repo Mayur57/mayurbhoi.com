97a61e875fd39db3bfc4f4d7fc0240fe225376f4
Mayur Bhoi <mayur072000@gmail.com>
2024-08-24 01:00:21 +0530

    reduce API calls

--- src/components/visitor.tsx
+++ src/components/visitor.tsx
@@ -1,23 +1,32 @@
 'use client'
 
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react'
+
+interface LocationContextType {
+  location: string | null
+}
+
+// Provide a default value to avoid TypeScript errors
+const LocationContext = createContext<LocationContextType>({
+  location: null,
+})
 
 export const LastVisitor = () => {
-  const [location, setLocation] = useState('Loading...');
+  const [location, setLocation] = useState('Loading...')
 
   useEffect(() => {
     const fetchLocation = async () => {
       try {
         // Fetch the previous location
-        const res = await fetch('/api/location');
-        const { location } = await res.json();
-        setLocation(location);
+        const res = await fetch('/api/location')
+        const { location } = await res.json()
+        setLocation(location)
 
         // Fetch current location and store in KV
-        const ipRes = await fetch('http://ip-api.com/json');
-        const ipData = await ipRes.json();
+        const ipRes = await fetch('http://ip-api.com/json')
+        const ipData = await ipRes.json()
         if (ipData.status === 'success') {
-          const { city, country } = ipData;
+          const { city, country } = ipData
           await fetch('/api/location', {
             method: 'POST',
             headers: {
@@ -25,19 +34,19 @@ export const LastVisitor = () => {
               'Access-Control-Allow-Origin': '*',
             },
             body: JSON.stringify({ city, country }),
-          });
+          })
         }
       } catch (error) {
-        console.error('Error fetching or updating location:', error);
+        console.error('Error fetching or updating location:', error)
       }
-    };
+    }
 
-    fetchLocation();
-  }, []);
+    fetchLocation()
+  }, [])
 
   return (
-    <p className='text-sm opacity-50 select-none'>
-      Last visit from {location}
-    </p>
-  );
-};
+    <LocationContext.Provider value={{ location }}>
+      <p className='text-sm opacity-50 select-none'>Last visit from {location}</p>
+    </LocationContext.Provider>
+  )
+}
