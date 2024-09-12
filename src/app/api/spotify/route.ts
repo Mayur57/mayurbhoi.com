8b76c12c09c219d99bcb73a8c13006a9869c3148
Mayur Bhoi <mayur072000@gmail.com>
2024-09-12 23:20:44 +0530

    fix: add dynamic revalidation

--- src/app/api/spotify/route.ts
+++ src/app/api/spotify/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
+export const revalidate = 0;
+
 const client_id = process.env['SPOTIFY_CLIENT_ID'];
 const client_secret = process.env['SPOTIFY_CLIENT_SECRET'];
 const refresh_token = process.env['SPOTIFY_REFRESH'];
