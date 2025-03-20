8f8acf2ab2b50844338779d3c27af055d3b5b2e0
Mayur Bhoi <mayur072000@gmail.com>
2025-03-20 18:14:16 +0530

    fix: headers and shadows

---
+++ src/components/blur-header.tsx
@@ -0,0 +1,38 @@
+export default function BlurHeader() {
+  return (
+    <>
+      <div
+        className='fixed top-0 w-full h-14 backdrop-blur-sm z-10'
+        style={{
+          maskImage:
+            'linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 100%)',
+          WebkitMaskImage:
+            'linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 100%)',
+        }}></div>
+      <div
+        className='fixed top-0 w-full h-14 backdrop-blur-sm z-20'
+        style={{
+          maskImage:
+            'linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 100%)',
+          WebkitMaskImage:
+            'linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 100%)',
+        }}></div>
+      <div
+        className='fixed top-0 w-full h-14 backdrop-blur-sm z-30'
+        style={{
+          maskImage:
+            'linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 100%)',
+          WebkitMaskImage:
+            'linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 100%)',
+        }}></div>
+              <div
+        className='fixed top-0 w-full h-14 backdrop-blur-sm z-40'
+        style={{
+          maskImage:
+            'linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 100%)',
+          WebkitMaskImage:
+            'linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 100%)',
+        }}></div>
+    </>
+  )
+}
