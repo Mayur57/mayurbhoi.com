cbd22ba3f648ec6b8a3d672058549d4bc903ea43
Mayur Bhoi <mayur072000@gmail.com>
2025-07-13 16:12:04 +0530

    fix: remove redundancies

--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -3,7 +3,7 @@ import Image, { StaticImageData } from 'next/image'
 export const Photo = ({ src }: { src: StaticImageData }) => {
   return (
     <Image
-      className='not-prose inline-block z-5 h-24 w-24 mb-4 rounded-xl object-cover saturate-0 contrast-125 border border-[#DADADA] dark:border-[#333] hover:h-48 hover:w-48 hover:saturate-100 hover:contrast-100 hover:rounded-2xl duration-500 ease-[cubic-bezier(.4,0,.2,1)] [will-change:width,height]'
+      className='not-prose inline-block z-5 h-24 w-24 mb-4 rounded-xl object-cover saturate-0 contrast-125 border border-[#DADADA] dark:border-[#333] hover:h-48 hover:w-48 hover:saturate-100 hover:contrast-100 hover:rounded-2xl duration-500 ease-[cubic-bezier(.4,0,.2,1)]'
       priority
       src={src}
       sizes='(max-width: 768px) 213px, 33vw'
